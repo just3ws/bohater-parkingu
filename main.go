f6a767853392a8bb2b6eb21dfe0007af60a421ee
Mike Hall <mike@just3ws.com>
2018-06-09 17:24:28 -0500

    WIP

--- main.go
+++ main.go
@@ -4,48 +4,24 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/julienschmidt/httprouter"
 )
 
-func indexHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	setCors(w)
-	fmt.Fprintf(w, "MAYBE THIS IS THE RESTFUL API")
+// Index endpoint
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Welcome!\n")
 }
 
-// used for COR preflight checks
-func corsHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	setCors(w)
-}
-
-// util
-func getFrontendURL() string {
-	return "http://localhost:8080"
-}
-
-func setCors(w http.ResponseWriter) {
-	frontendURL := getFrontendURL()
-	w.Header().Set("Access-Control-Allow-Origin", frontendURL)
-	w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS, POST, DELETE")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-}
-
-// Temporary Canary test to make sure Travis-CI is working
-func Canary(word string) string {
-	return word
+// Hello endpoint
+func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
 }
 
 func main() {
-	log.SetOutput(os.Stdout)
-
 	router := httprouter.New()
-	router.GET("/", indexHandler)
-	router.OPTIONS("/*any", corsHandler)
+	router.GET("/", Index)
+	router.GET("/hello/:name", Hello)
 
-	log.Println("Running api server in dev mode")
-
-	http.ListenAndServe(":8080", nil)
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
-
-// NOOO!
