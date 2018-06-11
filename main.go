49a8420fcb0970ff4078bdf3b933b6e25cdce3c5
Mike Hall <mike@just3ws.com>
2018-06-11 02:14:51 -0500

    Proceeding along

--- main.go
+++ main.go
@@ -2,49 +2,101 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
+	"net/url"
+	"os"
 	"time"
 
 	"github.com/just3ws/bohater-parkingu/models"
+	"github.com/sirupsen/logrus"
 )
 
-// // Index endpoint
-// func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-// 	fmt.Fprint(w, "Welcome!\n")
-// }
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	log.Debug("GET /")
 
-// // LookupRate endpoint
-// func LookupRate(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-// 	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
-// }
+	w.Header().Set("Content-Type", "application/json")
+
+	file, err := ioutil.ReadFile("static/spec.json")
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Fprintf(w, string(file))
+}
+
+func EndsParam(url *url.URL) (time.Time, error) {
+	params, ok := url.Query()["ends"]
+
+	if !ok || len(params) < 1 {
+		msg := "ends: param is missing"
+		return time.Time{}, errors.New(msg)
+	}
+
+	ends, err := time.Parse(time.RFC3339, params[0])
+	if err != nil {
+		msg := "ends: param is invalid"
+		return time.Time{}, errors.New(msg)
+	}
+
+	return ends, nil
+}
+
+func StartsParam(url *url.URL) (time.Time, error) {
+	params, ok := url.Query()["starts"]
+
+	if !ok || len(params) < 1 {
+		msg := "starts: param is missing"
+		return time.Time{}, errors.New(msg)
+	}
+
+	starts, err := time.Parse(time.RFC3339, params[0])
+	if err != nil {
+		msg := "starts: param is invalid"
+		return time.Time{}, errors.New(msg)
+	}
+
+	return starts, nil
+}
+
+func RateLookupHandler(w http.ResponseWriter, r *http.Request) {
+	log.Debug("GET /rate")
+
+	w.Header().Set("Content-Type", "application/json")
+
+	starts, err := StartsParam(r.URL)
+	if err != nil {
+		log.Error(err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	ends, err := EndsParam(r.URL)
+	if err != nil {
+		log.Error(err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	rate := models.Rate{Price: 1234, Starts: starts, Ends: ends}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(rate)
+}
+
+var log = logrus.New()
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
+	// log.SetOutput(os.Stdout)
+	log.Out = os.Stdout
+	log.Level = logrus.DebugLevel
+	log.Debug("Server starting...")
 
-		file, err := ioutil.ReadFile("static/spec.json")
-
-		if err != nil {
-			panic(err)
-		}
-
-		fmt.Fprintf(w, string(file))
-	})
-
-	http.HandleFunc("/rate", func(w http.ResponseWriter, r *http.Request) {
-		var starts time.Time
-		var ends time.Time
-
-		starts, _ = time.Parse(time.RFC3339, "2016-08-29T08:00:00.000Z")
-		ends, _ = time.Parse(time.RFC3339, "2016-08-29T09:00:00.000Z")
-
-		rate := models.Rate{Price: 1234, Starts: starts, Ends: ends}
-
-		json.NewEncoder(w).Encode(rate)
-	})
+	http.HandleFunc("/", IndexHandler)
+	http.HandleFunc("/rate", RateLookupHandler)
 
 	log.Fatal(http.ListenAndServe(":8888", nil))
 }
